8e4edad45942d281442097e66e64d5b6b3667a0e
Joseph Sams <sams_josep@yahoo.com>
2017-06-13 14:07:15 -0400

    Added transition to hero image

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -3,8 +3,10 @@
 (function ($) {
 
   var headlineLockup = $('.headline__lockup');
+  var heroCoverPhoto = $('.hero__cover-photo');
 
   $(window).on('load', function () {
+    heroCoverPhoto.removeClass('blur-hero-image');
     headlineLockup.removeClass('hide-headline');
   });
 })(jQuery);
